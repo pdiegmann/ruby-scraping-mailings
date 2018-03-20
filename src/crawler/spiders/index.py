79df87b8ada979c2684f5429ec11e3098bf864bf
Phil Diegmann <Phil@phil.local>
2018-03-20 12:25:55 +0100

    prepared spider for reading thread_url and id and store it as a json

--- src/crawler/spiders/index.py
+++ src/crawler/spiders/index.py
@@ -3,7 +3,7 @@ import scrapy
 
 class IndexSpider(scrapy.Spider):
     name = 'index'
-    start_urls = ['http://blade.nagaokaut.ac.jp/ruby/ruby-core/index.shtml'] # todo parameterization
+    #start_urls = ['http://blade.nagaokaut.ac.jp/ruby/ruby-core/index.shtml'] # todo parameterization
 
     def start_requests(self):
         for url in self.start_urls:
@@ -11,8 +11,9 @@ class IndexSpider(scrapy.Spider):
 
     def parse(self, response):
         for cell in response.css('td'):
-            for thread_link in cell.css('a::attr(href)').extract():
-                print thread_link
+            for thread_link in cell.css('a'):
+
                 yield {
-                    'thread_url': urlparse.urljoin(response.url, thread_link)
+                    'id': thread_link.css('::text').extract_first(),
+                    'thread_url': urlparse.urljoin(response.url, thread_link.css('::attr(href)').extract_first())
                     }
