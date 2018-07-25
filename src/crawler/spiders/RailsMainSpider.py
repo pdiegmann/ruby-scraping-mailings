d03a0ab3acac69b63415da4c0676a9d54134cbda
Phil Diegmann <Phil@Phil.home.phildiegmann.com>
2018-07-25 19:51:11 +0200

    updated crawling and git ignore

--- src/crawler/spiders/RailsMainSpider.py
+++ src/crawler/spiders/RailsMainSpider.py
@@ -24,12 +24,22 @@ class RubyLangMainSpider(scrapy.Spider):
             yield response.follow(last_link.css('::attr(href)').extract_first(), self.parse_thread_list)
 
     def parse_thread(self, response):
+        forum = ''
+        thread = ''
+        id = ''
+
         for message_raw in response.css('body > table > tr'):
-            message_link_parts = message_raw.css('td.subject > a::attr(href)').extract_first().split('/')
+            message_link = message_raw.css('td.subject > a::attr(href)').extract_first()
+            if message_link is not None:
+                message_link_parts = message_link.split('/')
+            else:
+                message_link_parts = [forum, thread, response.url]
+
             subject = message_raw.css('td.subject > a::text').extract_first()
             user = self.remove_html(message_raw.css('td.author').extract_first())
             date = message_raw.css('td.lastPostDate::text').extract_first()
-            text = message_raw.css('td.snippet > div > div').extract_first()
+            html = message_raw.css('td.snippet > div > div').extract_first()
+            text = self.remove_html(html)
             forum = message_link_parts[-3]
             thread = message_link_parts[-2]
             id = message_link_parts[-1]
@@ -41,8 +51,10 @@ class RubyLangMainSpider(scrapy.Spider):
                 'user': user,
                 'date': date,
                 'subject': subject,
+                'html': html,
                 'text': text
             }
 
     def remove_html(self, string):
-        return re.sub(re.compile('<.*?>'), '', string).strip()
+        if string is None: return ''
+        return re.sub(re.compile('<.*?>'), '', unicode(string)).strip()
